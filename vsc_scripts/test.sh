accc5175fe43d3b8fe29adb941c67926c9e755d0
tle <tuan.le@geo.tuwien.ac.at>
2019-01-18 16:23:05 +0100

    add VSC test script

---
+++ vsc_scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH -A p71008
+#SBATCH -J tle
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --array=1-5
+#SBATCH --time 00:10:00
+#SBATCH --licenses="gpfs@eodc"
+#SBATCH --qos=devel_0128
+####SBATCH --qos=normal_0256
+#SBATCH --partition=mem_0128
+####SBATCH --partition=mem_0256
+##########SBATCH --mail-type=BEGIN    # first have to state the type of event to occur
+##########SBATCH --mail-user=<email@address.at>   # and then your email address
+
+
+echo "=================================================="
+date
+echo "--------------------------------------------------"
+source /home/lv70533/tle/miniconda3/bin/activate rt1_env_test
+python -u /eodc/private/tuwgeo/users/tle/code_new/rt1_s1_processing/rt1_input_loadstackparallel_pj7.py /eodc/private/tuwgeo/users/tle/code_new/rt1_s1_processing/config_pj7.ini -totalarraynumber 5 -arraynumber ${SLURM_ARRAY_TASK_ID}
+echo "--------------------------------------------------"
+date
+echo "=================================================="
