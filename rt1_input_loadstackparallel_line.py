43120cfc93f01ab4e104e28b91afc419b652fb35
Tuan Le <tuan.le@geo.tuwien.ac.at>
2019-01-23 10:40:58 +0100

    fix ndvi reader in read_stack_line (read block idx change to line op)

--- rt1_input_loadstackparallel_line.py
+++ rt1_input_loadstackparallel_line.py
@@ -19,220 +19,6 @@ from scipy.signal import savgol_filter
 import multiprocessing as mp
 
 
-def read_stack(sig0_dir, plia_dir, block_size, cr_list, output_dir, ndvi_dir=None, orbit_direction=''):
-    '''
-    Read sig0 and plia rasters stack into a virtual raster stack, then read the time-series block-by block
-    Parameters
-    ----------
-    sig0_dir: str
-        directory of sig0 tif images
-    plia_dir: str
-        directory of plia tif images
-    block_size: int
-        block size: the size of the block. e.g if you would like to take 100x100m block, blocksize is 10
-    cr_list: list of list
-        list of block upper left locations. e.g:[[0,0],[1,1]]
-
-    Returns
-    -------
-
-    '''
-
-    random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-
-    # read ndvi stack if ndvi_dir exist
-    if ndvi_dir:
-        files_ndvi = os.listdir(ndvi_dir)
-        filelist_ndvi = []
-        times_ndvi = []
-        for fil in files_ndvi:
-            if 'NDVI300' in fil and fil.endswith('.tif'):
-                filelist_ndvi.append(os.path.join(ndvi_dir, fil))
-                times_ndvi.append(datetime.strptime(fil[19:27], "%Y%m%d"))
-    else:
-        ndvi_stack = None
-
-    # prepare timelist and filelist for sig0 reader
-    files_sig0 = os.listdir(sig0_dir)
-    filelist_sig0 = []
-    times_sig0 = []
-    sig0_pattern = 'IWGRDH1VV%s' % orbit_direction
-
-    for fil in files_sig0:
-        if 'SIG' in fil and sig0_pattern in fil and fil.endswith('.tif'):
-            filelist_sig0.append(os.path.join(sig0_dir, fil))
-            times_sig0.append(datetime.strptime(fil[1:16], "%Y%m%d_%H%M%S"))
-
-    # prepare timelist and filelist for plia reader
-    files_plia = os.listdir(plia_dir)
-    filelist_plia = []
-    times_plia = []
-    plia_pattern = 'IWGRDH1--%s' % orbit_direction
-
-    for fil in files_plia:
-        if 'PLIA' in fil and plia_pattern in fil and fil.endswith('.tif'):
-            filelist_plia.append(os.path.join(plia_dir, fil))
-            times_plia.append(datetime.strptime(fil[1:16], "%Y%m%d_%H%M%S"))
-
-    # raise a warning if number of sig0 and plia is not equal
-    if len(times_sig0) != len(times_plia):
-        print("Warning! The number of sig0 and plia images is not equal")
-
-    # check if there is any files in sig0 which the correspond plia doesn't exist
-    for time in times_sig0:
-        if time not in times_plia:
-            idx_sig0_no_plia = times_sig0.index(time)
-            del times_sig0[idx_sig0_no_plia]
-            del filelist_sig0[idx_sig0_no_plia]
-            print("Warning! The scene in this date ", time, " doesn't have the correspond plia. Removing...")
-
-    # read sig and plia blocks
-    for [c, r] in cr_list:
-        # don't re-process files that already exist
-        if str(c) + '_' + str(r) + '.dump' in os.listdir(output_dir):
-            print(str(c) + '_' + str(r), 'already processed...')
-            continue
-
-        # read ndvi to virtual stack
-        if ndvi_dir:
-            ndvi_stack = create_imagestack_dataset(name=random_name + 'ndvi', filelist=filelist_ndvi, times=times_ndvi,
-                                                   nodata=-9999)
-
-        # read sig0 to virtual stack
-        sig_stack = create_imagestack_dataset(name=random_name + 'SIG', filelist=filelist_sig0, times=times_sig0,
-                                              nodata=-9999)
-        # read plia to virtual stack
-        plia_stack = create_imagestack_dataset(name=random_name + 'LIA', filelist=filelist_plia, times=times_plia,
-                                               nodata=-9999)
-
-        sig0_block = sig_stack.read_ts(c * block_size, r * block_size, block_size, block_size)
-        plia_block = plia_stack.read_ts(c * block_size, r * block_size, block_size, block_size)
-        time_sig0_list = sig0_block[0]
-        data_sig0_list = sig0_block[1]
-        time_plia_list = plia_block[0]
-        data_plia_list = plia_block[1]
-        final_list = []
-        for time in time_sig0_list:
-            idx_sig0 = time_sig0_list.index(time)
-            idx_plia = time_plia_list.index(time)
-            # loop over slice
-            for row in range(block_size):
-                for col in range(block_size):
-                    sig0_value = data_sig0_list[idx_sig0][row][col]
-                    plia_value = data_plia_list[idx_plia][row][col]
-                    final_list.append([time, sig0_value, plia_value])
-
-        df = pd.DataFrame(final_list)
-        df.columns = ['time', 'sig', 'inc']
-
-        # nan handling
-        df = df.replace(-9999, np.nan)
-
-        df = df.dropna()
-
-        # true value
-        df['sig'] = df['sig'].div(100)
-        df['inc'] = df['inc'].div(100)
-
-        # convert to radian
-        df['inc'] = np.deg2rad(df['inc'])
-
-        # set index
-        df = df.set_index('time')
-
-        # sort by index
-        df.sort_index(inplace=True)
-
-        df = inpdata_inc_average(df)
-
-        # -----------------------------------------
-
-        if ndvi_stack:
-            ndvi_block = ndvi_stack.read_ts(c * block_size, r * block_size, block_size, block_size)
-            time_ndvi_list = ndvi_block[0]
-            data_ndvi_list = ndvi_block[1]
-            ndvi_list = []
-
-            for time in time_ndvi_list:
-                idx_ndvi = time_ndvi_list.index(time)
-                # loop over slice
-                for row in range(block_size):
-                    for col in range(block_size):
-                        ndvi_value = data_ndvi_list[idx_ndvi][row][col]
-                ndvi_list.append([time, np.mean(ndvi_value)])
-
-            df_ndvi = pd.DataFrame(ndvi_list)
-            df_ndvi.columns = ['time', 'ndvi']
-
-            # nan handling
-            df_ndvi = df_ndvi.replace(list(range(251, 256)), np.nan)
-
-            df_ndvi = df_ndvi.dropna()
-
-            # convert to physical value
-            df_ndvi['ndvi'] = df_ndvi['ndvi'] / 250 - 0.08
-
-            # set index
-            df_ndvi = df_ndvi.set_index('time')
-
-            # sort by index
-            df_ndvi.sort_index(inplace=True)
-
-        # ------------------------ RQ's manipulation
-        '''
-        manual_dyn_df = pd.DataFrame(df.index.month.values.flatten(), df.index, columns=['VOD'])
-        fixed_vals = {
-            # --- specification of volume-scattering phase function
-            'v_forward': [False, .4],  # asymmetry factor of forward-peak
-            'v_bounceoff': [False, .4],  # asymmetry factor of bounceoff-peak
-            'alpha': [False, .5],  # fraction of forward / backward peak intensity
-            'isopart': [False, .5],  # fraction of isotropic / directional peak intensity
-            'v1': [False, 0.0],  # minimum optical depth value
-            'v2': [False, 1.],  # maximum optical depth value
-            # --- specification of surface-scattering phase function
-            'aa': [False, .4],  # surface a-parameter
-            's1': [False, 0.0],  # minimum NormBRDF value
-        }
-        defdict_vars = {
-            'SM': [True, 0.25, 'D', ([0.01], [1.])],
-            'VOD': [True, .25, 'manual', ([0.05], [1.]), manual_dyn_df],
-            'bsf': [False, 0.0, None, ([0.01], [.25])],
-            'tt': [False, 0.2, None, ([0.05], [0.6])],
-            'omega': [True, 0.1, None, ([0.1], [0.6])],
-            's2': [False, 0.25, None, ([0.15], [0.3])],
-        }
-
-        defdict_i = {**fixed_vals, **defdict_vars}
-        '''
-
-        VOD_input = df_ndvi.resample('D').interpolate(method='nearest')
-        # get a smooth curve
-        # VOD_input = VOD_input.rolling(window=60, center=True, min_periods=1).mean()
-        VOD_input = VOD_input.clip_lower(0).apply(savgol_filter, window_length=61, polyorder=2).clip_lower(0)
-        # reindex to input-dataset
-        VOD_input = VOD_input.reindex(df.index.drop_duplicates()).dropna()
-        # ensure that there are no ngative-values appearing (possible due to rolling-mean and interpolation)
-        VOD_input = VOD_input.clip_lower(0)
-        # drop all measurements where no VOD estimates are available
-        df = df.loc[VOD_input.dropna().index]
-
-        # manual_dyn_df = pd.DataFrame(df.index.month.values.flatten(), df.index, columns=['VOD'])
-        defdict_i = {
-            'bsf': [False, 0.01, None, ([0.01], [.25])],
-            'v': [False, 0.4, None, ([0.01], [.4])],
-            'v2': [True, 1., None, ([0.5], [1.5])],
-            'VOD': [False, VOD_input.values.flatten()],
-            'SM': [True, 0.25, 'D', ([0.05], [0.5])],
-            # 'VOD'   : [True, 0.25,'30D', ([0.01], [1.])],
-            'frac': [True, 0.5, None, ([0.01], [1.])],
-            'omega': [True, 0.3, None, ([0.05], [0.6])],
-        }
-
-        # TODO: pass ndvi df to out_dict
-        out_dict = {'dataset': df, 'defdict': defdict_i, '_fnevals_input': None, 'c': c, 'r': r, 'outdir': output_dir}
-        parallelfunc(out_dict)
-
-
 def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_dir=None, orbit_direction=''):
     '''
     Read sig0 and plia rasters stack into a virtual raster stack, then read the time-series block-by block
@@ -244,8 +30,6 @@ def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_
         directory of plia tif images
     block_size: int
         block size: the size of the block. e.g if you would like to take 100x100m block, blocksize is 10
-    cr_list: list of list
-        list of block upper left locations. e.g:[[0,0],[1,1]]
 
     Returns
     -------
@@ -326,6 +110,13 @@ def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_
         data_sig0_list = sig0_block[1]
         time_plia_list = plia_block[0]
         data_plia_list = plia_block[1]
+
+        if ndvi_stack:
+            ndvi_block = ndvi_stack.read_ts(0, line * block_size, 10000, block_size)
+            time_ndvi_list = ndvi_block[0]
+            data_ndvi_list = ndvi_block[1]
+
+
         final_list = []
         for px in range(1000):  # number of pixel per line
             for time in time_sig0_list:
@@ -364,17 +155,13 @@ def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_
             # -----------------------------------------
 
             if ndvi_stack:
-                #TODO: read ndvi
-                ndvi_block = ndvi_stack.read_ts(0, line * block_size, 10000, block_size)
-                time_ndvi_list = ndvi_block[0]
-                data_ndvi_list = ndvi_block[1]
+                # TODO: read ndvi
                 ndvi_list = []
-
                 for time in time_ndvi_list:
                     idx_ndvi = time_ndvi_list.index(time)
                     # loop over slice
                     for row in range(block_size):
-                        for col in range(block_size):
+                        for col in range(px * block_size, (px + 1) * block_size):
                             ndvi_value = data_ndvi_list[idx_ndvi][row][col]
                     ndvi_list.append([time, np.mean(ndvi_value)])
 
@@ -396,32 +183,7 @@ def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_
                 df_ndvi.sort_index(inplace=True)
 
             # ------------------------ RQ's manipulation
-            '''
-            manual_dyn_df = pd.DataFrame(df.index.month.values.flatten(), df.index, columns=['VOD'])
-            fixed_vals = {
-                # --- specification of volume-scattering phase function
-                'v_forward': [False, .4],  # asymmetry factor of forward-peak
-                'v_bounceoff': [False, .4],  # asymmetry factor of bounceoff-peak
-                'alpha': [False, .5],  # fraction of forward / backward peak intensity
-                'isopart': [False, .5],  # fraction of isotropic / directional peak intensity
-                'v1': [False, 0.0],  # minimum optical depth value
-                'v2': [False, 1.],  # maximum optical depth value
-                # --- specification of surface-scattering phase function
-                'aa': [False, .4],  # surface a-parameter
-                's1': [False, 0.0],  # minimum NormBRDF value
-            }
-            defdict_vars = {
-                'SM': [True, 0.25, 'D', ([0.01], [1.])],
-                'VOD': [True, .25, 'manual', ([0.05], [1.]), manual_dyn_df],
-                'bsf': [False, 0.0, None, ([0.01], [.25])],
-                'tt': [False, 0.2, None, ([0.05], [0.6])],
-                'omega': [True, 0.1, None, ([0.1], [0.6])],
-                's2': [False, 0.25, None, ([0.15], [0.3])],
-            }
-    
-            defdict_i = {**fixed_vals, **defdict_vars}
-            '''
-
+            # TODO: what if we don't have ndvi?
             VOD_input = df_ndvi.resample('D').interpolate(method='nearest')
             # get a smooth curve
             # VOD_input = VOD_input.rolling(window=60, center=True, min_periods=1).mean()
@@ -448,7 +210,7 @@ def read_stack_line(sig0_dir, plia_dir, block_size, line_list, output_dir, ndvi_
             # TODO: pass ndvi df to out_dict
             out_dict = {'dataset': df, 'defdict': defdict_i, '_fnevals_input': None, 'c': px, 'r': line,
                         'outdir': output_dir}
-            print('processed: ',px, line, datetime.now())
+            print('processed: ', px, line, datetime.now())
             # parallelfunc(out_dict)
 
 
@@ -676,23 +438,14 @@ def main(args, test_vsc_param=False):
             # return async_res
 
 
-def read_stack_mp(process_dict):
-    read_stack(sig0_dir=process_dict['sig0_dir'],
-               plia_dir=process_dict['plia_dir'],
-               block_size=process_dict['block_size'],
-               cr_list=process_dict['cr_list'],
-               output_dir=process_dict['output_dir'],
-               ndvi_dir=process_dict['ndvi_dir'],
-               orbit_direction=process_dict['orbit_direction'])
-
 def read_stack_line_mp(process_dict):
     read_stack_line(sig0_dir=process_dict['sig0_dir'],
-               plia_dir=process_dict['plia_dir'],
-               block_size=process_dict['block_size'],
-               line_list=process_dict['cr_list'],
-               output_dir=process_dict['output_dir'],
-               ndvi_dir=process_dict['ndvi_dir'],
-               orbit_direction=process_dict['orbit_direction'])
+                    plia_dir=process_dict['plia_dir'],
+                    block_size=process_dict['block_size'],
+                    line_list=process_dict['cr_list'],
+                    output_dir=process_dict['output_dir'],
+                    ndvi_dir=process_dict['ndvi_dir'],
+                    orbit_direction=process_dict['orbit_direction'])
 
 
 # -------------------------------------------------
@@ -791,30 +544,6 @@ if __name__ == '__main__':
     sys.argv.append("1")
 
     print("Start", datetime.now())
-    async_res = main(sys.argv[1:], test_vsc_param=False)
+    main(sys.argv[1:], test_vsc_param=False)
 
-    # print('gogogo')
-    # initialize a stdout0 array for comparison
-
-    #    import time as tme
-    #    vectorlen = np.vectorize(len)
-    #
-    #    stdout0 = np.array(async_res.stdout)
-    #    while not async_res.ready():
-    #        #ids = async_res.engine_id
-    #        stdout1 = np.array(async_res.stdout)
-    #        where = stdout1 != stdout0
-    #        if np.any(where):
-    #            oldlens = vectorlen(stdout0[where])
-    #            changedstdouts = ['Kernel '
-    #                              #+ str(ids[i]) + ':  '
-    #                              + str(np.arange(len(where))[where][i]) + ':  '
-    #                              + '\n'
-    #                              + val[oldlength:]
-    #                              for i, [val, oldlength] in enumerate(zip(stdout1[where], oldlens))]
-    #
-    #            sys.stdout.write('\r ' + ' '.join(changedstdouts))
-    #            sys.stdout.flush()
-    #            stdout0 = stdout1
-    #            tme.sleep(.5)
     pass
